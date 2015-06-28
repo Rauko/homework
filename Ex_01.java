e7a435c7e9988aa46231ff3dca14d291b1ff7e82
Ra'Uko <raukotaur@gmail.com>
2015-06-28 11:26:48 +0300

    simplified code, added checking the total number of input arguments, displays error in the case of mismatch the minimum requirements.

--- Ex_01.java
+++ Ex_01.java
@@ -1,28 +1,18 @@
 public class Ex_01 {
 	public static void main(String[] args) {
-	    //inputting A
-	    Integer A = 0;
-	    try {
-	        A = Integer.valueOf(args[0]);    
-		System.out.println("Your variable A: " + A);
-	    } catch (NumberFormatException e) {  
-	        System.err.println("Wrong string format! Enter number to A.");  
-	    } 
-	    //inputting B
-	    Integer B = 0;
-	    try {
-	        B = Integer.valueOf(args[1]);   
-		System.out.println("Your variable B: " + B + ". Now let's swap 'em!"); 
-	    } catch (NumberFormatException e) {  
-	        System.err.println("Wrong string format! Enter number to B.");  
-	    } 
+		if (args.length >= 2) {
+			Integer A = Integer.parseInt(args[0]);
+			System.out.println("Your variable A: " + A + ";");
 		
-	    //swapping
-	    int buf;
-	    buf = A;
-	    A = B;
-	    B = buf;
+			Integer B = Integer.parseInt(args[1]);
+			System.out.println("Your variable B: " + B + ". Now let's swap 'em!");
+
+			int buf = A;
+			A = B;
+			B = buf;
 		
-	    System.out.println("Now your variables are: A=" + A + ", B=" + B + ".");	
+			System.out.println("Now your variables are: A=" + A + ", B=" + B + ".");
+		} else
+			System.err.println("Enter two digit arguments!");
 	}
 }
--- Ex_02.java
+++ Ex_02.java
@@ -1,39 +1,24 @@
 public class Ex_02 {
 	public static void main(String[] args){
-	    //inputting A
-	    Integer A = 0;
-	    try {
-	        A = Integer.valueOf(args[0]);    
-		System.out.println("Your variable A: " + A);
-	    } catch (NumberFormatException e) {  
-	        System.err.println("Wrong string format! Enter number to A.");  
-	    } 
-	    //inputting B
-	    Integer B = 0;
-	    try {
-	        B = Integer.valueOf(args[1]);   
-		System.out.println("Your variable B: " + B + "."); 
-	    } catch (NumberFormatException e) {  
-	        System.err.println("Wrong string format! Enter number to B.");  
-	    } 
-	    //inputting C
-	    Integer C = 0;
-	    try {
-	        C = Integer.valueOf(args[2]);    
-		System.out.println("Your variable C: " + C);
-	    } catch (NumberFormatException e) {  
-	        System.err.println("Wrong string format! Enter number to C.");  
-	    } 
+		if (args.length >= 3) {
+			Integer A = Integer.parseInt(args[0]);
+			System.out.println("Your variable A: " + A);
 
-		System.out.println("Your variables are: A=" + A + ", B=" + B + ", C=" + C + ". Now let's swap 'em!");
+			Integer B = Integer.parseInt(args[1]);
+			System.out.println("Your variable B: " + B + ".");
+		
+			Integer C = Integer.parseInt(args[2]);
+			System.out.println("Your variable C: " + C);
+
+			System.out.println("Your variables are: A=" + A + ", B=" + B + ", C=" + C + ". Now let's change 'em plases!");
 			
-		//changing
-		int buf;
-		buf = A;
-		A = B;
-		B = C;
-		C = buf;
+			int buf = A;
+			A = B;
+			B = C;
+			C = buf;
 			
-		System.out.println("Now your variables are: A=" + A + ", B=" + B + ", C=" + C + ".");
+			System.out.println("Now your variables are: A=" + A + ", B=" + B + ", C=" + C + ".");
+		} else
+			System.err.println("Enter three digit arguments!");
 	}
 }
