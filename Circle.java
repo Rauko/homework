443e747356f6d06173f152b7578cdfd37f6d73d4
Ra'Uko <raukotaur@gmail.com>
2015-06-28 09:47:56 +0300

    moved Pi and L vars out of if. Now it containes only output strings

--- Circle.java
+++ Circle.java
@@ -2,9 +2,9 @@ public class Circle {
 	public static void main(String[] args){
 		if (args.length >= 1){
 			Integer d = Integer.parseInt(args[0]);
+			double Pi = 3.14d;
+			double L = d * Pi; 
 			if (d > 0) {
-				double Pi = 3.14d;
-				double L = d * Pi; 
 				System.out.println("L = " + L + ".");
 			} else  {
 				System.err.println("You enter null argument for d. In that case L = " + L + ".");
