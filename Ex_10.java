745e3558899bdb49da71d52451c0e2308ff9ba81
Ra'Uko <raukotaur@gmail.com>
2015-06-30 08:59:59 +0300

    Ready for review

--- Ex_10.java
+++ Ex_10.java
@@ -4,11 +4,32 @@
 
 public class Ex_10 {
 	public static void main(String[] args) {
-		Integer var = Integer.parseInt(args[0]);
-		int dec = 1;
-		int ones = 1;
-		dec = var / 10;
-		ones = var % 10;  
-		System.out.println("десятков " + dec + ", единиц " + ones);
+		if (args.length >= 1){
+			if (args.length > 1)
+				System.err.println("You entered more arguments, than programm needs. \nIt will use only first one.");
+			Integer var = Integer.parseInt(args[0]);
+			if (var == 0){
+				System.err.println("You entered 0. Enter 10-99 and start again.");
+				return;
+			} else {
+				if ((var > 0 && var < 10) || (var > -10 && var < 0)) {
+					System.err.println("You entered 0-9. Enter 10-99 and start again.");
+					return;
+				} else {
+					if ((var > 9 && var < 100) || (var > -100 && var < -9)) {
+						int tens = 1;
+					//	int ones = 1;
+						tens = var / 10;
+						var %= 10;  
+						System.out.println("Entered digit consists of " + tens + " tens and " + var + " ones.");
+					} else {
+						if (var >= 100){
+							System.err.println("You entered 100+. Enter 10-99 and start again.");
+						} else System.err.println("You entered digit less than -100. Enter 10-99 and start again.");
+					return;
+					}
+				}
+			}			
+		}
 	}
 }
