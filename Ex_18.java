6e2dfe5d9b6d95214e90f1215cb339df7b751be0
Ra'Uko <raukotaur@gmail.com>
2015-07-03 09:04:20 +0300

    Ready for review.corrected mistakes in 19(15) and 18(13-14). if (sec >= 60){

--- Ex_18.java
+++ Ex_18.java
@@ -10,19 +10,17 @@ public class Ex_18 {
 				System.err.println("You entered more arguments, than programm needs. \nIt will use only first one.");
 			Integer ones = Integer.parseInt(args[0]);
 			System.out.println("You entered digit: " + ones);
-			int tens = ones;
-			int hunds = ones;
 			if (ones >= 100 && ones <= 999) {
-				hunds /= 100;
-				tens = (tens%100 - ones%10)/10;
+				int hunds = ones/100;
+				int tens = (ones%100 - ones%10)/10;
 				ones = ones - hunds*100 - tens*10;
-				System.out.println(hunds + " " + tens + " " + ones);
+				System.out.println("It consists of digits: " + hunds + ", " + tens + ", " + ones + ".");
 				if (hunds < tens && tens < ones) {
 					System.out.println("This three-digit number forms an increasing arithmetic sequence.");
 				} else {
 					System.out.println("This three-digit number doesn't form an increasing arithmetic sequence.");
 					System.err.println("Сondition is not satisfied!");
-				} 
+				}
 			} else System.err.println("Entered digit doesn't satisfy condition. \nEnter a value in array from 100 to 999");
 		} else System.err.println("You haven't entered an argument!");
 	}
--- Ex_19.java
+++ Ex_19.java
@@ -9,17 +9,15 @@ public class Ex_19 {
 				System.err.println("You entered more arguments, than programm needs. \nIt will use only first one.");
 			Integer var = Integer.parseInt(args[0]);
 			System.out.println("You entered digit: " + var);
-			int rav = 0;
 			if (var >= 1000 && var <= 9999) {
-				rav = var%100;
-				var = (var - rav) /100;
-				rav = (rav%10)*10 + rav/10;
+				int rav = ((var%100)%10)*10 + (var%100)/10;
+				var /= 100;
 				if (var == rav) {
 					System.out.println("This four-digit number is a palindrome.");
 				} else {
 					System.out.println("This four-digit number isn't a palindrome.");
 					System.err.println("Сondition is not satisfied!");
-				} 
+				}
 			} else System.err.println("Entered digit doesn't satisfy condition. \nEnter a value in array from 1000 to 9999");
 		} else System.err.println("You haven't entered an argument!");
 	}
