3c7a23f2282719e79121a0bb105e80bbe1b7a1e5
Ra'Uko <raukotaur@gmail.com>
2015-07-06 23:17:18 +0300

    Class Work

---
+++ Cl_w_1.java
@@ -0,0 +1,10 @@
+public class Cl_w_1 {
+	public static void main(String[] args) {
+		int N = 1;// int N;
+		//N = 1;
+		while (N <= 32) {
+			N *=2; //N = 2 * N;
+			System.out.println(N);
+		}
+	}
+}
