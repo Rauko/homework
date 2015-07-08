6b82884ed4706585d54865bb39c0ee58b6cb541a
Ra'Uko <raukotaur@gmail.com>
2015-07-08 22:18:36 +0300

    Class work. 04/07/2015

---
+++ Cl_w_2.java
@@ -0,0 +1,13 @@
+public class Cl_w_2 {
+	public static void main(String[] args){
+	//	int x, y;
+	//	x = 5;
+	//	y = 1;
+		int x = 5, y = 1;
+		while (x > 0){
+			x -= 1; // x = x - 1;
+			y *= x; // y = y * x;
+			System.out.println(y);
+		}
+	}
+}
