198b3eda8306646a30a86e2c879cd90dc950904b
Ra'Uko <raukotaur@gmail.com>
2015-07-20 23:20:22 +0300

    Ready for review.

--- Ex_48.java
+++ Ex_48.java
@@ -16,6 +16,18 @@ public class Ex_48 {
 		return smallest;
 	}
 
+	public static int searchOfSmallest(int a, int b, int c){
+		int smallest = a;
+		if(smallest > b && smallest > c){
+			if (b < c){
+				smallest = b;
+			}else smallest = c;
+		} else if (smallest < b && smallest > c){
+			smallest = c;
+		} else smallest = b;
+		return smallest;
+	}
+
 	public static int[] randomArrayCreate(int N){
 		int sizeArr = N;
 		int maxVal = 100;
@@ -30,15 +42,20 @@ public class Ex_48 {
 
 	public static void main(String[] args){
 		if (args.length > 0) System.out.println("This programm don't needs any arguments!");
-		
-		Integer N = 3;
-
-		int[] array = randomArrayCreate(N);
-
-		System.out.println(Arrays.toString(array));
-
-		int smallest = searchOfSmallest(array);
-
-		System.out.println("Product of all arrays elements is " + smallest + ".");
+		int smallest = 0;
+		if (args.length == 3) {
+			System.out.println("You entered 3 arguments, programm will work in manual mode!");
+			Integer a = Integer.parseInt(args[0]);
+			Integer b = Integer.parseInt(args[1]);
+			Integer c = Integer.parseInt(args[2]);
+			System.out.println("["+ a + ", " + b + ", " + c + "]");
+			smallest = searchOfSmallest(a, b, c);
+		} else {
+			Integer N = 3;
+			int[] array = randomArrayCreate(N);
+			System.out.println(Arrays.toString(array));
+			smallest = searchOfSmallest(array);
+		}
+		System.out.println("The smallest element is: " + smallest + ".");
 	}
 }
