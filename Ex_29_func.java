25ec2474d526262d6aa829d152d225165512e2ff
Ra'Uko <raukotaur@gmail.com>
2015-07-22 13:05:24 +0300

    Ready for review.

--- Ex_29_func.java
+++ Ex_29_func.java
@@ -1,12 +1,12 @@
 /*
-Даны два целых числа A и B (A < B). Найти сумму всех целых чисел от A до B включительно. Для реализации использовать функции.
+Даны два целых числа A и B (A < B). Найти сумму всех целых чисел от A до B включительно. 
+Для реализации использовать функции.
 */
 
-public class Ex_29_func {
-	
+public class Ex_29_funk {
 	public static int subSum(int A, int B){
 		int sum = 0;
-		for(int i = A+1; i <= B; i++)
+		for(int i = A; i <= B; i++)
 			sum += i;
 		return sum;
 	}
@@ -19,18 +19,22 @@ public class Ex_29_func {
 		}
 		return cond;
 	}
+	
+	public static boolean generalCond(String[] args, int conditionLength){
+		boolean cond = false;
+		if (args.length < conditionLength) {
+			System.err.println("You haven't entered arguments! The task requires entering two arguments.");
+			cond = true;
+		}
+		if (args.length > conditionLength) System.out.println("You entered more arguments, than programm needs. \nIt will use only first two.");
+		return cond;
+	}
 
 	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.err.println("You haven't entered arguments! The task requires entering two arguments.");
-			return;
-		}
-		if (args.length > 2) System.out.println("You entered more arguments, than programm needs. \nIt will use only first two.");
+		if(generalCond(args,2)) return;
 		Integer A = Integer.parseInt(args[0]);
 		Integer B = Integer.parseInt(args[1]);
 		if (cond(A,B)) return;
-		System.out.print("A = " + A + ", B = " + B + ". \nThe sum of all integers from A to B inclusive: ");
-		int sum = subSum(A,B);
-		System.out.println(sum);
+		System.out.print("A = " + A + ", B = " + B + ". \nThe sum of all integers from A to B inclusive: " + subSum(A,B));
 	}
 }
