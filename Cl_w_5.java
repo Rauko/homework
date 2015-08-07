44849aefea5607a7bcf283e6b91bde32c6bca7b3
Ra'Uko <raukotaur@gmail.com>
2015-08-07 11:01:22 +0300

    Sorted

--- Cl_w_5.java
+++
@@ -1,9 +0,0 @@
-public class Cl_w_5 {
-	public static void main(String[] args) {
-		Integer x = Integer.parseInt(args[0]);
-		x %= 2;
-		if(x == 0) {
-			System.out.println("even");
-		} else System.out.println("odd");
-	}
-}
